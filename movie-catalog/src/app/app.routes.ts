e42cfbc0eaed7f86e8b7417de3b7356285462b63
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2026-04-19 22:58:58 +0300

    Create auth components - login and register, layout and home
    Implement routing

--- movie-catalog/src/app/app.routes.ts
+++ movie-catalog/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home/home.js';
+import { Login } from './features/auth/login/login.js';
+import { Register } from './features/auth/register/register.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+];
--- movie-catalog/src/app/app.ts
+++ movie-catalog/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header.js';
+import { Footer } from './layout/footer/footer.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('movie-catalog');
 }

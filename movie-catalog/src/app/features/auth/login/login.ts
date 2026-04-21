af2280d3a3aa656827658c836e7cfc3ee0f54877
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2026-04-21 09:15:15 +0300

    Reform login using Reactive Forms; build directives and validators

--- movie-catalog/src/app/features/auth/login/login.ts
+++ movie-catalog/src/app/features/auth/login/login.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { InputError } from '../../../shared/directives/input-error.directive.js';
+import { AuthService } from '../../../core/services/auth.service.js';
+import { NotificationService } from '../../../core/services/notification.service.js';
+import { EmailValidator } from '../../../shared/directives/email-validator.directive.js';
+import { emailValidator } from '../../../shared/validators/email.validator.js';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, FormsModule, InputError],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  private notifService = inject(NotificationService);
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, emailValidator()]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+  });
+
+  isLoading = false;
+
+  onLogin(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({ email, password }).subscribe({
+      next: (user) => {
+        this.authService.setSession(user);
+        this.isLoading = false;
+        this.notifService.showSuccess('Login successful');
+        this.router.navigate(['/themes']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+      },
+    });
+  }
+}

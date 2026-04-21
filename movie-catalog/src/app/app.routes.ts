d69db823e77a7cbf5a08f21636f3cce86a7d8b5c
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2026-04-21 12:39:02 +0300

    Lazy loading

--- movie-catalog/src/app/app.routes.ts
+++ movie-catalog/src/app/app.routes.ts
@@ -12,18 +12,53 @@ import { authGuard } from './core/guards/auth.guard.js';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  { path: 'home', loadComponent: () => import('./features/home/home/home.js').then((c) => c.Home) },
 
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.js').then((c) => c.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.js').then((c) => c.Register),
+  },
 
-  { path: 'movies', component: Movies },
-  { path: 'movies/:movieId', component: MovieContent },
-  { path: 'add-movie', component: NewMovie, canActivate: [authGuard] },
-  { path: 'movies/edit/:movieId', component: EditMovie, canActivate: [authGuard] },
+  {
+    path: 'movies',
+    loadComponent: () => import('./features/movies/movies.js').then((c) => c.Movies),
+  },
+  // { path: 'movies', component: Movies },
+  {
+    path: 'movies/:movieId',
+    loadComponent: () =>
+      import('./features/movies/movie-content/movie-content.js').then((c) => c.MovieContent),
+  },
+  // { path: 'movies/:movieId', component: MovieContent },
+  {
+    path: 'add-movie',
+    loadComponent: () => import('./features/movies/new-movie/new-movie.js').then((c) => c.NewMovie),
+    canActivate: [authGuard],
+  },
+  // { path: 'add-movie', component: NewMovie, canActivate: [authGuard] },
+  // { path: 'movies/edit/:movieId', component: EditMovie, canActivate: [authGuard] },
+  {
+    path: 'movies/edit/:movieId',
+    loadComponent: () =>
+      import('./features/movies/edit-movie/edit-movie.js').then((c) => c.EditMovie),
+    canActivate: [authGuard],
+  },
 
-  { path: 'profile', component: Profile, canActivate: [authGuard] },
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/profile.js').then((c) => c.Profile),
+    canActivate: [authGuard],
+  },
 
+  // { path: 'profile', component: Profile, canActivate: [authGuard] },
 
-  { path: '**', component: NotFound },
+  // { path: '**', component: NotFound },
+  {
+    path: '**',
+    loadComponent: () => import('./features/not-found/not-found.js').then((c) => c.NotFound),
+  },
 ];

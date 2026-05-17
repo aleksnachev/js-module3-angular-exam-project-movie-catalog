1607aa741ca03b3800f381b0936f2255ea696499
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2026-05-17 12:03:40 +0300

    Fix interceptors issue

--- movie-catalog/src/app/app.config.ts
+++ movie-catalog/src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptor.js';
+import { errorInterceptor } from './core/interceptors/error.interceptor.js';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({eventCoalescing:true}),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
-  ]
+    provideHttpClient(withInterceptors([authInterceptor,errorInterceptor]))]
 };
